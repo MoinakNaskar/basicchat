ee5f7dc0ff9d7a28a1327af77405a59938536e01
Moinak Naskar <mainaknaskar408@gmail.com>
2023-07-14 16:07:44 +0530

    step6

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -74,9 +74,9 @@ class _LoginViewState extends State<LoginView> {
                 devtools.log(UserCredential.toString());
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
-                  devtools.log('User not found');
+                  await showErrorDialog(context, 'User-not-found');
                 } else if (e.code == 'wrong-password') {
-                  devtools.log("wrong password");
+                  await showErrorDialog(context, 'Wrong credentials');
                 }
               }
             },
@@ -95,4 +95,26 @@ class _LoginViewState extends State<LoginView> {
       ),
     );
   }
+}
+
+Future<void> showErrorDialog(
+  BuildContext context,
+  String text,
+) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('An error occured'),
+        content: Text(text),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('ok'))
+        ],
+      );
+    },
+  );
 }
